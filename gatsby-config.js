a5425312e1964760c83f032c8b8886c1dd282e89
khaliliali <ali.khalili@outlook.com>
2019-04-10 12:59:56 +0430

    small changes

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Regular Joe's`,
+    title: `Ali's Coffee`,
     description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`
+    author: `@khaliliali`
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
